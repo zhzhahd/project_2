4ec0c33cc6a898f82069ce73a460dc5f163c4591
zhzhahd <y990303@naver.com>
2023-01-10 17:59:50 +0900

    end-2

--- js/main.js
+++ js/main.js
@@ -39,22 +39,22 @@ $(document).ready(function () {
                  $('#second p').css('transform', 'translateY(60px)').css('opacity', '0');
                  $('.rose h1').css('transform', 'translateY(0px)').css('opacity', '1');
                  $('#men').css('transform', 'translateY(0px)').css('opacity', '1');
-                 $('#gift h1').css('transform', 'translateX(-100px)').css('opacity', '0');
+                 $('.gift h1').css('transform', 'translateX(-100px)').css('opacity', '0');
                  
                  
              }                        
 
-             var gift = $('#gift').offset().top;
+             var gift = $('.gift').offset().top;
              if (ws > gift - 300) {
                  $('.rose h1').css('transform', 'translateY(20px)').css('opacity', '0');
                  $('#men').css('transform', 'translateY(200px)').css('opacity', '0');
-                 $('#gift h1').css('transform', 'translateX(0px)').css('opacity', '1'); 
+                 $('.gift h1').css('transform', 'translateX(0px)').css('opacity', '1'); 
                  
              }
 
              var a5 = $('.a5').offset().top;
              if (ws > a5 - 900) {
-                 $('#gift h1').css('transform', 'translateX(-20px)').css('opacity', '0'); 
+                 $('.gift h1').css('transform', 'translateX(-20px)').css('opacity', '0'); 
              }
 
          });
